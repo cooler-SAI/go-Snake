576960382391a9da7a9c7de48e7a65934af9256b
cooler-SAI <cooler19860212@yahoo.com>
2025-02-10 21:47:51 +0300

    logger added in project

--- logger/logger.go
+++ logger/logger.go
@@ -1,1 +1,12 @@
 package logger
+
+import (
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+	"os"
+)
+
+func Init() {
+	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
+	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
+}

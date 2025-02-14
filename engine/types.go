807fc61bd2bf6b55930a04a7b37e5fc7e099f94d
cooler-SAI <cooler19860212@yahoo.com>
2025-02-14 21:20:59 +0300

    types struct added

--- engine/types.go
+++ engine/types.go
@@ -1,8 +1,29 @@
 package engine
 
-// import "github.com/hajimehoshi/ebiten/v2"
+type Direction int
+
+const (
+	DirectionUp Direction = iota
+	DirectionDown
+	DirectionLeft
+	DirectionRight
+)
+
+type Snake struct {
+	X, Y  int       // Текущая позиция головы
+	Body  [][2]int  // Сегменты тела
+	Dir   Direction // Направление
+	Speed int       // Скорость (пикселей в кадр)
+}
+
+type Food struct {
+	X, Y int // Позиция еды
+}
 
 type Game struct {
 	ScreenWidth  int
 	ScreenHeight int
+	Snake        *Snake
+	Food         *Food
+	GameOver     bool
 }
